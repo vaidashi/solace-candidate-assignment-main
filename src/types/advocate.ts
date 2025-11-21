9c0d8751aaee54e6c0f0ffb1c68ee3fa31017527
vaidashi <vaid50@yahoo.com>
2025-11-21 16:45:23 -0500

    initial frontend load fix, search filtering for most fields

---
+++ src/types/advocate.ts
@@ -0,0 +1,11 @@
+export interface Advocate {
+    id?: number;
+    firstName: string;
+    lastName: string;
+    city: string;
+    degree: string;
+    specialties: string[];
+    yearsOfExperience: number;
+    phoneNumber: number;
+    createdAt?: string;
+};
