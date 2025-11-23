c541617f8f91d17efe0078205998e1c404861f29
vaidashi <vaid50@yahoo.com>
2025-11-23 15:45:59 -0500

    connect to actual db, fix setup config for db, connection pooling, pagination

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,9 +1,15 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
+import { NextRequest } from "next/server";
 
-export async function POST() {
-  const records = await db.insert(advocates).values(advocateData).returning();
+export async function POST(request: NextRequest) {
+  try {
+    const records = await db.insert(advocates).values(advocateData).returning();
 
-  return Response.json({ advocates: records });
+    return Response.json({ advocates: records });
+  } catch (error) {
+    console.error("Error seeding advocates:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,19 +1,25 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import type { PostgresJsDatabase
+ } from "drizzle-orm/postgres-js";
 
 const setup = () => {
   if (!process.env.DATABASE_URL) {
     console.error("DATABASE_URL is not set");
     return {
       select: () => ({
-        from: () => [],
+        from: () => Promise.resolve([]),
       }),
+      insert: () => ({ values: () => ({ returning: () => Promise.resolve([]) }) }),
     };
   }
 
   // for query purposes
-  const queryClient = postgres(process.env.DATABASE_URL);
-  const db = drizzle(queryClient);
+  const queryClient = postgres(process.env.DATABASE_URL, {
+    max: 5, // set connection pool size to 5
+    idle_timeout: 60, // close idle clients after 60 seconds
+  });
+  const db: PostgresJsDatabase = drizzle(queryClient);
   return db;
 };
 
